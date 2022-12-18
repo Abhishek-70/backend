1d782230eedfe5494d0b84317045381c90d684be
m-triple-m <triplem656@mail.com>
2022-12-18 18:52:39 +0530

    added login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-import Home from './components/Home';
-import Login from './components/Login';
-import Register from './components/Register';
-import { BrowserRouter , Routes, Route, Link } from 'react-router-dom';
-import Header from './components/Header';
-
-
-
+import logo from "./logo.svg"
+import "./App.css"
+import Home from "./components/Home"
+import Login from "./components/Login"
+import Register from "./components/Register"
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import Header from "./components/Header"
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-      <Header></Header>
-
-
-      {/*<Link to = "/home">Home</Link>
-      <Link to = "/login">Login</Link>
-  <Link  to = "/register">Register</Link>*/}
-          <Routes>
-            <Route element = {<Home></Home>} path = "home" />
-            <Route element = {   <Login></Login> } path = "login" />
-            <Route element = {   <Register></Register> } path = "register" />
-          
-            
-            
-            
-          </Routes>
+        <Header></Header>
+        <Routes>
+          <Route element={<Home></Home>} path="home" />
+          <Route element={<Login></Login>} path="login" />
+          <Route element={<Register></Register>} path="register" />
+        </Routes>
       </BrowserRouter>
     </div>
-    
-      
-  );
+  )
 }
 
-export default App;
+export default App
