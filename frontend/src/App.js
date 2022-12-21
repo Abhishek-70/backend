c18792f8502ba12d2c3831905aee20d1032ce7bb
m-triple-m <triplem656@mail.com>
2022-12-22 00:42:34 +0530

    ..

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,11 +7,13 @@ import { BrowserRouter, Routes, Route, Link, Navigate } from "react-router-dom"
 import Header from "./components/Header"
 import Track from "./components/Track"
 import AddIssue from "./components/AddIssue"
+import {Toaster} from 'react-hot-toast';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
+      <Toaster />
         <Header></Header>
         <Routes>
           <Route element={<Navigate to="/login" />} path="/" />
--- frontend/src/components/Track.js
+++ frontend/src/components/Track.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react"
+import { toast } from "react-hot-toast"
+import "./track.css"
 
 const Track = () => {
   const [issueList, setIssueList] = useState([])
@@ -13,6 +15,19 @@ const Track = () => {
   useEffect(() => {
     getDataFromBackend()
   }, [])
+
+  const showStatus = (status) => {
+    if (status.toLowerCase() === "pending") return <p className="badge badge-pending">Pending</p>
+    if (status.toLowerCase() === "solved") return <p className="badge badge-resolved">Resolved</p>
+    if (status.toLowerCase() === "re-opened") return <p className="badge badge-reopened">Re-Opened</p>
+  }
+
+  const showType = (type) => {
+    if (type.toLowerCase() === "bug") return <p className="badge badge-bug">Bug</p>
+    if (type.toLowerCase() === "wontfix") return <p className="badge badge-fix">Won't Fix</p>
+    if (type.toLowerCase() === "question") return <p className="badge badge-ques">Question</p>
+    if (type.toLowerCase() === "duplicate") return <p className="badge badge-dup">Duplicate</p>
+  }
 
   const displayData = () => {
     return (
@@ -32,17 +47,52 @@ const Track = () => {
           {issueList.map((issue) => (
             <tr>
               <td>{issue.title}</td>
-              <td>{issue.type}</td>
+              <td>{showType(issue.type)}</td>
               <td>{issue.assignedBy}</td>
               <td>{issue.assignedTo}</td>
-              <td>{issue.status}</td>
+              <td>{showStatus(issue.status)}</td>
               <td>{new Date(issue.createdAt).toLocaleDateString()}</td>
-              <td></td>
+              <td>
+                {!issue.resolved ? (
+                  <button className="btn btn-success" onClick={(e) => updateStatus("solved", issue._id)}>
+                    Resolve <i class="fas fa-check"></i>
+                  </button>
+                ) : issue.status === "re-opened" ? (
+                  <button className="btn btn-success" onClick={(e) => updateStatus("solved", issue._id)}>
+                    Resolve <i class="fas fa-check"></i>
+                  </button>
+                ) : (
+                  <button className="btn btn-primary" onClick={(e) => updateStatus("re-opened", issue._id)}>
+                    Re-Open <i class="fas fa-bug"></i>{" "}
+                  </button>
+                )}
+              </td>
             </tr>
           ))}
         </tbody>
       </table>
     )
+  }
+
+  const updateStatus = async (newStatus, id) => {
+    const res = await fetch("http://localhost:5000/issue/update/" + id, {
+      method: "PUT",
+      body: JSON.stringify({
+        status: newStatus,
+        resolved: true,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+    console.log(res.status)
+    if (res.status === 200) {
+      newStatus==='solved'?
+      toast.success("Issue Resolved")
+      :
+      toast.success("Issue Re-Opened")
+      getDataFromBackend()
+    }
   }
 
   return (
